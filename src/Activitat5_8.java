9ecc295954af22151e2ed3a7a903362254fb8021
Martín Peidro <peidromartin@gmail.com>
2022-12-12 17:25:53 +0100

    3

--- src/Activitat5_8.java
+++ src/Activitat5_8.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 public class Activitat5_8 {
     public static void main(String[] args) {
         int[] arrayA = {10, 20, 30, 40, 50};
@@ -8,11 +9,11 @@ public class Activitat5_8 {
         arrayC[1] = suma + arrayA[0];
         arrayC[0] = arrayA[0] * arrayA[1] * arrayA[2] * arrayA[3] * arrayA[4];
         arrayC[2] = arrayA[0] + arrayA[1] + arrayA[2] + arrayA[3] + arrayA[4];
-        arrayC[3] = arrayB[0] / arrayC[0];
+        arrayC[3] = arrayC[0] / arrayB[0];
         arrayC[4] = Math.max(arrayB[0], arrayA[4]);
 
-        System.out.println(Arrays.toString(arrayA))
-        System.out.println(Arrays.toString(arrayB))
+        System.out.println(Arrays.toString(arrayA));
+        System.out.println(Arrays.toString(arrayB));
         System.out.println(Arrays.toString(arrayC));
     }
 }
