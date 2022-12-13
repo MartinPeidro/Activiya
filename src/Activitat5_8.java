abf5779e74e507745b4322c58a506ba1aabe9e69
Martín Peidro <peidromartin@gmail.com>
2022-12-13 20:03:43 +0100

    Act8 corregida

--- src/Activitat5_8.java
+++ src/Activitat5_8.java
@@ -10,7 +10,7 @@ public class Activitat5_8 {
         arrayC[0] = arrayA[0] * arrayA[1] * arrayA[2] * arrayA[3] * arrayA[4];
         arrayC[2] = arrayA[0] + arrayA[1] + arrayA[2] + arrayA[3] + arrayA[4];
         arrayC[3] = arrayC[0] / arrayB[0];
-        arrayC[4] = Math.max(arrayB[0], arrayA[4]);
+        arrayC[4] = Math.max(arrayB[0], arrayA[arrayA.length-1]);
 
         System.out.println(Arrays.toString(arrayA));
         System.out.println(Arrays.toString(arrayB));
